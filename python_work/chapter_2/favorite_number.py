2d9b8aaec5167e90b49b04b2bd216e12e168f7b4
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-04-21 22:43:10 -0300

    [PYTHON] chapter-2 - feat: learned and did exercises about numbers, comments and the zen of python

---
+++ python_work/chapter_2/favorite_number.py
@@ -0,0 +1,5 @@
+# Gabriel Martins Padoin
+# This program prints the value stored in the favorite_number variable
+# along with a wrapping string
+favorite_number = 3
+print(f"My favorite number is the number {favorite_number}")
---
+++ python_work/chapter_2/name_cases.py
@@ -0,0 +1,8 @@
+# Gabriel Martins Padoin
+# This program stores a person's name in a variable
+# And then prints the person's name in lowercase, uppercase and title case
+person_name = "Gabriel martins padoin"
+
+print(person_name.lower())
+print(person_name.upper())
+print(person_name.title())
