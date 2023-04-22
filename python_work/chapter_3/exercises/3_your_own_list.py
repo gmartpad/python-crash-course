9063aca1095abc373b3b823e83071d06ae12ea00
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-04-22 04:26:29 -0300

    [PYTHON] chapter-3 - feat: read and did exercises for the intro to lists in chapter 3

---
+++ python_work/chapter_3/exercises/3_your_own_list.py
@@ -0,0 +1,5 @@
+cars = ['2023 Chevrolet Corvette', '2023 Toyota Supra', '2023 Audi R8']
+
+print(f'I would like to own a {cars[0]}')
+print(f'I would like to own a {cars[1]}')
+print(f'I would like to own a {cars[2]}')
