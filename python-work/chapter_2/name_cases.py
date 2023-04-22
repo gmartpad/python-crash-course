0417111a05476631fddd14fee413be1fd6e6cfaa
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-04-21 21:57:34 -0300

    [PYTHON] chapter-2 - feat: read and did exercises for Variables and Strings in chapter 2

---
+++ python-work/chapter_2/name_cases.py
@@ -0,0 +1,5 @@
+person_name = "Gabriel martins padoin"
+
+print(person_name.lower())
+print(person_name.upper())
+print(person_name.title())
---
+++ python-work/chapter_2/personal_message.py
@@ -0,0 +1,2 @@
+person_name = "Gabriel Martins Padoin"
+print(f"Hello {person_name}, would you like to learn some Python today?")
